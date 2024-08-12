edbabfb66164425467fb7467a4f3cb6077d78044
AdlerCastro <adlercastro01@gmail.com>
2024-08-11 21:07:34 -0300

    :lipstick: style: Estilizando a borda do top

--- desafio_01/src/components/organisms/Lista/components/Empty/index.tsx
+++ desafio_01/src/components/organisms/Lista/components/Empty/index.tsx
@@ -2,7 +2,7 @@ import { ClipboardList } from "lucide-react";
 
 export function Empty() {
   return (
-    <div className="flex h-60 flex-col items-center justify-center text-zinc-600 gap-4 border-t border-zinc-700">
+    <div className="flex h-60 flex-col items-center justify-center text-zinc-600 gap-4 border-t border-zinc-700 rounded-lg">
       <ClipboardList className="w-14 h-14" />
       <p className="text-zinc-500 w-80 text-center text-base">
         <strong>Você ainda não tem tarefas cadastradas</strong>
